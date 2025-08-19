33cf747aea33acbb64f3ed4a9d56a3312d4191e3
Brodecks <raphae.donikian@oulootk.fr>
2025-08-19 13:56:48 +0200

    phase 3 : mvc + outils (test)

---
+++ src/modele/JeuClient.java
@@ -0,0 +1,31 @@
+package modele;
+/**
+ * Gestion du jeu côté client
+ *
+ */
+public class JeuClient extends Jeu {
+	
+	/**
+	 * Controleur
+	 */
+	public JeuClient() {
+	}
+	
+	@Override
+	public void connexion() {
+	}
+
+	@Override
+	public void reception() {
+	}
+	
+	public void deconnexion() {
+	}
+	/**
+	 * Envoi d'une information vers le serveur
+	 * fais appel une fois à l'envoi dans la classe Jeu
+	 */
+	public void envoi() {
+	}
+
+}
